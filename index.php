2cb5de78560d9e44a795397e5e0ef7b52f224521
tbrah <thomasmessell@gmail.com>
2017-05-11 00:44:18 +0200

    Header gradient done / Header text in place

--- index.php
+++ index.php
@@ -11,7 +11,32 @@ $title = 'Intellifinder';
     </head>
 <body>
 
-	
+<!--Header-->
+<header>
+  <div class="header-background">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  <div class="container">
+    <nav>
+      <img src="/assets/img/logo.svg">
+      <ul>
+        <li><a href="#">CASES</a></li>
+        <li><a href="#">ABOUT US</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+    </nav>
+    <div class="row">
+      <div class="col-md-7">
+        <h1>IntelliFinder ID + can be used by everyone</h1>
+        <p>A simple location application that can be used by all to get the work done to perfection. Simply download the app, and all of your work is on the cloud and can be controlled in the backend by co-workers.</p>
+        <button type="button" class="btn btn-success">EXPLORE THE STACK</button>
+        <button type="button" class="btn btn-default">LOG IN</button>
+      </div>
+    </div>
+  </div>
+</header>
 
 </body>
 </html>
