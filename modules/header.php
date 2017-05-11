67ab678dd14f915d2633b370839ed933725d2c35
Mikkel Selko <mikkelrise@gmail.com>
2017-05-11 14:35:07 +0200

    Made it juicy🔥

--- modules/header.php
+++ modules/header.php
@@ -7,30 +7,29 @@
   </div>
   <div class="container">
     <nav>
-      <img src="<?php echo $prefix; ?>assets/img/logo.svg">
+      <a href="<?php echo $prefix; ?>index.php"><img class="noselect" src="<?php echo $prefix; ?>assets/img/logo.svg"></a>
       <ul>
-        <li><a href="#">CASES</a></li>
-        <li><a href="#">ABOUT US</a></li>
-        <li><a href="#">CONTACT</a></li>
+        <li><a class="noselect" href="#">CASES</a></li>
+        <li><a class="noselect" href="#">ABOUT US</a></li>
+        <li><a class="noselect" href="#">CONTACT</a></li>
       </ul>
     </nav>
     <div class="row">
       <div class="col-md-7">
         <h1>IntelliFinder ID + can be used by everyone</h1>
         <p>A simple location application that can be used by all to get the work done to perfection. Simply download the app, and all of your work is on the cloud and can be controlled in the backend by co-workers.</p>
-        <a type="button" class="btn btn-success">EXPLORE THE STACK</a>
+        <a type="button" class="btn btn-success">EXPLORE THE APP</a>
         <a type="button" class="btn btn-default">LOG IN</a>
 
-        <a target="_blank" href="https://itunes.apple.com/dk/app/intellifinder-id-plus/id979128138?l=da&mt=8"><img class="get-it" src="<?php echo $prefix; ?>assets/img/app-google-store/app.png"></a>
-        <a target="_blank" href="https://play.google.com/store/apps/details?id=eu.intellifinder&hl=da"><img class="get-it" src="<?php echo $prefix; ?>assets/img/app-google-store/google.png"></a>
+        <a target="_blank" href="https://itunes.apple.com/dk/app/intellifinder-id-plus/id979128138?l=da&mt=8"><img class="get-it noselect" src="<?php echo $prefix; ?>assets/img/app-google-store/app.png"></a>
+        <a target="_blank" href="https://play.google.com/store/apps/details?id=eu.intellifinder&hl=da"><img class="get-it noselect" src="<?php echo $prefix; ?>assets/img/app-google-store/google.png"></a>
 
       </div>
     
       <div class="col-md-5">
-        <div class="device phone">
-
+        <div class="device phone pull-right">
+          <img src="<?php echo $prefix; ?>assets/img/app-guts.svg" alt="IntelliFinder">
         </div>
-
-    </div>
+      </div>
   </div>
 </header>
--- modules/intro.php
+++ modules/intro.php
@@ -1,22 +1,30 @@
 <section id="intro">
 	
 	<div class="container">
-	
+
      	<div class="row">
 
-			<h2 class="text-align-right">The complete app for <br>intelligent business</h2>
+	     	<div class="col-md-6">
 
-			<p class="text-align-right">We believe that payments is a problem rooted in code,<br> 
-			not finance. We obsessively seek out elegant, composable <br>
-			abstractions that enable robust, scalable, flexible integrations. <br>
-			Because we eliminate needless complexity and extraneous <br>
-			details, you can get up and running with Stripe in just a <br>
-			couple of minutes.We believe that payments is a problem <br>
-			rooted in code, not finance. We obsessively seek out elegant, <br>
-			composable abstractions that enable robust, scalable, flexible <br>
-			integrations. Because we eliminate needless complexity and <br>
-			extraneous details, you can get up and running with Stripe in <br>
-			just a couple of minutes.</p>
+				<h2 class="text-align-right">The complete app for <br>intelligent business</h2>
+
+				<p class="text-align-right">We believe that payments is a problem rooted in code,<br> 
+				not finance. We obsessively seek out elegant, composable <br>
+				abstractions that enable robust, scalable, flexible integrations. <br>
+				Because we eliminate needless complexity and extraneous <br>
+				details, you can get up and running with Stripe in just a <br>
+				couple of minutes.We believe that payments is a problem <br>
+				rooted in code, not finance. We obsessively seek out elegant, <br>
+				composable abstractions that enable robust, scalable, flexible <br>
+				integrations. Because we eliminate needless complexity and <br>
+				extraneous details, you can get up and running with Stripe in <br>
+				just a couple of minutes.</p>
+
+			</div>
+
+			<div class="device tablet pull-right">
+	          <img src="<?php echo $prefix; ?>assets/img/app-guts-ipad.svg" alt="IntelliFinder">
+	        </div>
 
 		</div>
 
