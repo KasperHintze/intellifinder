d102863a3a83d8d0b4e6366841341e1dceefd8f7
tbrah <thomasmessell@gmail.com>
2017-05-13 00:04:55 +0200

    footer elements in place and looking nizzle

--- modules/footer.php
+++ modules/footer.php
@@ -1,5 +1,48 @@
 <footer>
-	
-	
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+
+				<img class="location-img" src="<?php echo $prefix; ?>assets/img/locationPointer.svg">
+
+				<div class="in-touch-wrapper">
+
+					<h2>Ready to get in touch?</h2>
+					<p>- Write an e-mail or give us a call</p>
+
+					<span><i class="fa fa-facebook"></i></span>
+					<span><i class="fa fa-instagram"></i></span>
+					<span><i class="fa fa-twitter"></i></span>
+
+				</div>
+
+			</div>
+			<div class="col-md-6">
+
+				<i class="fa fa-angle-left"></i>
+
+				<div class="footer-card contact-details">
+
+					<h5>IntelliFinder A/S</h5>
+					<p>Kærvej 39</p>
+					<p>5220 Odense SØ</p>
+
+					<p>Telefon: +45 44 22 08 88</p>
+					<p>E-mail: info@intellifinder.dk</p>
+					<p>CVR: 30 28 26 71</p>
+
+				</div>
+				<div class="footer-card map">
+
+
+
+				</div>
+
+				<i class="fa fa-angle-right"></i>
+
+			</div>
+		</div>
+	</div>
 
 </footer>
