2418a7031cf41219c0febe78529011042770f5ef
Mikkel Selko <mikkelrise@gmail.com>
2017-05-12 02:56:14 +0200

    Changes to gitignore

--- modules/tour.php
+++ modules/tour.php
@@ -1,1 +1,27 @@
-<section class="tour"></section>
+<section id="tour">
+    
+    <div class="container">
+
+     	<div class="row">
+            
+            <div class="device tablet pull-left">
+	          <img src="<?php echo $prefix; ?>assets/img/app-guts-ipad.svg" alt="IntelliFinder">
+	        </div>
+
+	     	<div class="col-md-6">
+
+	     		<div class="featureWrapper">
+	     		    
+                    <div class="feature">
+                        
+                    </div>
+                    
+	     		</div>
+
+			</div>
+
+		</div>
+
+	</div>
+    
+</section>
